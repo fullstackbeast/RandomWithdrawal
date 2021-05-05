eb4aa5b4c31a2d7429dd9cac180ec216e7f314d2
Omolola Toyyib Babatunde <dpoohwinnie0@gmail.com>
2021-05-05 15:39:39 +0100

    Complete App

---
+++ RandomWithdrawal/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RandomWithdrawal
+{
+    class Program
+    {
+
+        static double currentBalance;
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            double openingBalance = random.Next(5000, 10000);
+            
+            currentBalance = openingBalance;
+
+            Console.WriteLine($"OpeningBalance is {openingBalance}");
+
+            for(; currentBalance > 0;){
+                double withdrawalAmount = random.Next(1, 100);
+                withrawal(withdrawalAmount);
+            }
+
+        }
+
+         static void withrawal(double withrawalAmount){
+            currentBalance -= withrawalAmount;
+            Console.WriteLine($"A withrawal of {withrawalAmount} made");
+            Console.WriteLine($"New Balance is: {currentBalance} ");
+        }
+    }
+}
